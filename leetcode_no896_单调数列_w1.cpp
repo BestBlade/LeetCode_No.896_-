8d8d83966a241b221c1959aab15f177be55454c6
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-28 16:03:58 +0800

    Add files via upload

---
+++ leetcode_no896_单调数列_w1.cpp
@@ -0,0 +1,102 @@
+﻿/* ------------------------------------------------------------------|
+如果数组是单调递增或单调递减的，那么它是单调的。
+
+如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+
+当给定的数组 A 是单调数组时返回 true，否则返回 false。
+
+ 
+
+示例 1：
+
+输入：[1,2,2,3]
+输出：true
+示例 2：
+
+输入：[6,5,4,4]
+输出：true
+示例 3：
+
+输入：[1,3,2]
+输出：false
+示例 4：
+
+输入：[1,2,4,5]
+输出：true
+示例 5：
+
+输入：[1,1,1]
+输出：true
+ 
+
+提示：
+
+1 <= A.length <= 50000
+-100000 <= A[i] <= 100000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/monotonic-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：68 ms, 在所有 C++ 提交中击败了87.14%的用户
+*	内存消耗：48.3 MB, 在所有 C++ 提交中击败了85.30%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool isMonotonic(vector<int>& A) {
+    bool inc = 1;
+    bool dec = 1;
+
+    for (int i = 1; i < A.size(); i++) {
+        if (A[i] > A[i - 1]) {
+            dec = 0;
+        }
+        if (A[i] < A[i - 1]) {
+            inc = 0;
+        }
+    }
+
+    return inc || dec;
+}
